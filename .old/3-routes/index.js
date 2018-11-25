95d81e687cca6634560aed02a23e4f2a8b94d795
Alex de Wekker <alexdewekker6@gmail.com>
2018-11-25 20:22:15 +0100

    moved all files to .old folder, starting postgresql conversion

---
+++ .old/3-routes/index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+const mongodb = require("mongodb");
+const exec = require("child_process").exec;
+
+// Set up mongodb connect url.
+const user = process.env.MONGODB_RWU;
+const ww = process.env.MONGODB_RWP;
+const mongoport = 27017;
+const ip = "192.168.1.90";
+const db = "de_kelder";
+const collinks = "links";
+const MongoClient = mongodb.MongoClient;
+//const mongourl = "mongodb://" + user + ":" + ww + "@" + ip + ":" + mongoport + "/" + db;
+const mongourl = "mongodb://localhost:27017/de_kelder";
+
+// --------------- GET  PAGES ---------------
+
+// Function to easily get the page.
+function getPage(url, view, title) {
+    router.get(url, (req, res, next) => {
+        res.render(view, { title : title });
+    });
+}
+
+// The landing page.
+router.get("/", (req, res, next) => {
+    // Get the hostname from the url.
+    var urlbar = req.headers.host.split(":");
+    MongoClient.connect(mongourl, (err, db) => {
+        if (err) {
+            console.log("Unable to connect to the server");
+        } else {
+            console.log("Connection established");
+            var linkcol = db.collection(collinks);
+            linkcol.find({}).sort({url:1}).toArray((err, result) => {
+                if (err) {
+                    res.render("error", { // Change to error pug later.
+                        error : err
+                    });
+                } else if (result.length) {
+                    res.render("index", {
+                        links : result,
+                        // Pass through the hostname to easily switch between env.
+                        url : urlbar[0]
+                    });
+                } else {
+                    res.render("error", { // Change to error later.
+                        error : "No documents found"
+                    });
+                }
+                db.close();
+            });
+        }
+    });
+});
+
+// Wake my PC.
+router.get("/wol", (req, res, next) => {
+    exec("wolpc", (err, stdout, stderr) => {
+        if (err) {
+            console.log(err);
+            res.redirect("/");
+        } else if (stderr) {
+            console.log(stderr);
+            res.redirect("/");
+        } else {
+            console.log(stdout);
+            res.redirect("/");
+        }
+    })
+})
+
+// Just get a simple page.
+getPage("/test", "index", "Test");
+
+router.get("/test2", (req, res, next) => {
+    res.send("Dit is test 2 bitch!");
+});
+
+module.exports = router;
---
+++ .old/de_kelder.js
@@ -0,0 +1,46 @@
+// -------------- SET UP STUFF --------------
+
+// Required packages.
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+
+// Call the routers and eclare the app.
+const indexRouter = require("./3-routes/index");
+const app = express();
+
+// Stop anything from being shown about the server.
+app.disable("x-powered-by");
+
+// Set the folder for the pug files and make the source pretty.
+app.set("views", path.join(__dirname, "0-views"));
+app.set("view engine", "pug");
+app.locals.pretty = true;
+
+
+// --------------- MIDDLEWARE ---------------
+
+// Set the public folder
+app.use(express.static(path.join(__dirname, "1-public")));
+// Set the favicon.
+app.use(favicon(path.join(__dirname, "1-public", "favicon.png")));
+
+// Use the routes.
+app.use("/", indexRouter);
+
+// 404 errors.
+app.use( (req, res, next) => {
+    next(createError(404));
+});
+
+// Error handler.
+app.use( (err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page.
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+module.exports = app;
