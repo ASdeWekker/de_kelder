007d30a8cc8bf4aa47835b9ecf5fea124a902381
Alex de Wekker <alexdewekker6@gmail.com>
2018-11-25 21:55:11 +0100

    added the code for postgres but not getting into the server

--- 3-routes/index.js
+++ 3-routes/index.js
@@ -1,18 +1,41 @@
 const express = require("express");
 const router = express.Router();
-const mongodb = require("mongodb");
+// const mongodb = require("mongodb");
+const { Pool, Client } = require("pg");
 const exec = require("child_process").exec;
 
 // Set up mongodb connect url.
-const user = process.env.MONGODB_RWU;
-const ww = process.env.MONGODB_RWP;
-const mongoport = 27017;
-const ip = "192.168.1.90";
-const db = "de_kelder";
-const collinks = "links";
-const MongoClient = mongodb.MongoClient;
-//const mongourl = "mongodb://" + user + ":" + ww + "@" + ip + ":" + mongoport + "/" + db;
-const mongourl = "mongodb://localhost:27017/de_kelder";
+// const user = process.env.MONGODB_RWU;
+// const ww = process.env.MONGODB_RWP;
+// const mongoport = 27017;
+// const ip = "192.168.1.90";
+// const db = "de_kelder";
+// const collinks = "links";
+// const MongoClient = mongodb.MongoClient;
+// //const mongourl = "mongodb://" + user + ":" + ww + "@" + ip + ":" + mongoport + "/" + db;
+// const mongourl = "mongodb://localhost:27017/de_kelder";
+
+// Set up the postgres connect url.
+const user = process.env.PSQLU;
+const ww = process.env.PSQLW;
+const host = "192.168.1.90";
+const psqlport = 5432;
+const db = "dekelder";
+// const connStr = "postgresql://" + user + ":" + ww + "@" + host + ":" + psqlport + "/" + db;
+// Create a new client.
+// const client = new Client({
+//     connectionString: connStr
+// });
+const client = new Client({
+    user: user,
+    host: host,
+    database: db,
+    password: ww,
+    port: psqlport
+});
+client.connect();
+// Make a query.
+const query = "select * from projects";
 
 // --------------- GET  PAGES ---------------
 
@@ -23,41 +46,56 @@ function getPage(url, view, title) {
     });
 }
 
-// The landing page.
 router.get("/", (req, res, next) => {
-    // Get the hostname from the url.
-    var urlbar = req.headers.host.split(":");
-    MongoClient.connect(mongourl, (err, db) => {
-        if (err) {
-            console.log("Unable to connect to the server");
-        } else {
-            console.log("Connection established");
-            var linkcol = db.collection(collinks);
-            linkcol.find({}).sort({url:1}).toArray((err, result) => {
-                if (err) {
-                    res.render("error", { // Change to error pug later.
-                        error : err
-                    });
-                } else if (result.length) {
-                    res.render("index", {
-                        links : result,
-                        // Pass through the hostname to easily switch between env.
-                        url : urlbar[0]
-                    });
-                } else {
-                    res.render("error", { // Change to error later.
-                        error : "No documents found"
-                    });
-                }
-                db.close();
-            });
-        }
-    });
+    let url = req.headers.host.split(":");
+    client.query(query)
+        .then(data => res.render("index", {
+            links : data,
+            // Pass the ip address in the url to easily switch between hosts.
+            url : url[0]
+        }))
+        .catch(e => console.error(e.stack))
+        .catch(res.send("Unable to connect or find anything."));
+    client.end();
 });
+
+// The landing page.
+// router.get("/", (req, res, next) => {
+//     // Get the hostname from the url.
+//     var urlbar = req.headers.host.split(":");
+//     MongoClient.connect(mongourl, (err, db) => {
+//         if (err) {
+//             console.log("Unable to connect to the server");
+//         } else {
+//             console.log("Connection established");
+//             var linkcol = db.collection(collinks);
+//             linkcol.find({}).sort({url:1}).toArray((err, result) => {
+//                 if (err) {
+//                     res.render("error", { // Change to error pug later.
+//                         error : err
+//                     });
+//                 } else if (result.length) {
+//                     res.render("index", {
+//                         links : result,
+//                         // Pass through the hostname to easily switch between env.
+//                         url : urlbar[0]
+//                     });
+//                 } else {
+//                     res.render("error", { // Change to error later.
+//                         error : "No documents found"
+//                     });
+//                 }
+//                 db.close();
+//             });
+//         }
+//     });
+// });
+
+getPage("/", "index", "Home");
 
 // Wake my PC.
 router.get("/wol", (req, res, next) => {
-    exec("wolpc", (err, stdout, stderr) => {
+    exec("/wol.sh", (err, stdout, stderr) => {
         if (err) {
             console.log(err);
             res.redirect("/");
@@ -70,12 +108,5 @@ router.get("/wol", (req, res, next) => {
         }
     })
 })
-
-// Just get a simple page.
-getPage("/test", "index", "Test");
-
-router.get("/test2", (req, res, next) => {
-    res.send("Dit is test 2 bitch!");
-});
 
 module.exports = router;
