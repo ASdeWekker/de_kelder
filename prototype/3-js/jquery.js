55e4cd7e20ce104d4cd5f6921d36d15d0f82fca2
Alex de Wekker <alexdewekker6@gmail.com>
2018-12-01 23:01:17 +0100

    added recipe stylesheets, did some more styling for the input bas and rearranged some elements

--- prototype/3-js/jquery.js
+++ prototype/3-js/jquery.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".item-title").click(function() {
-        $(".item div p").toggleClass("show");
+    $(".item-title-link").click(function() {
+        $(".small-desc-link").css({color: "red"});
     });
 });
