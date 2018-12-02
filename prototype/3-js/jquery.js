74791494cba6d1e9f07c3b63251b973e3cd0fb78
Alex de Wekker <alexdewekker6@gmail.com>
2018-12-02 21:58:52 +0100

    added a sliding animation to the small description, started with single recipe page

--- prototype/3-js/jquery.js
+++ prototype/3-js/jquery.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $(".item-title-link").on("click", function() {
-        $(this).closest("a").siblings().find("p").toggleClass("show");
+        $(this).closest("a").siblings().find("p").slideToggle(200);
     });
 });
