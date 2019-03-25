be1ef01d26b6d110c3b3ffcde354853afc2fc4e6
Alex de Wekker <alexdewekker6@gmail.com>
2019-03-25 22:39:44 +0100

    populate it, not yet working i think

--- ping.py
+++ ping.py
@@ -1,2 +1,14 @@
 #!/bin/python3
 
+import requests
+import os
+
+hostname = "192.168.1.77"
+response = os.system("ping -c 1 " + hostname)
+
+if response ==0:
+    pingstatus = "Pcaats is on"
+    # doe iets
+else:
+    pingstatus = "Pcaats is off"
+    # doe nog iets
