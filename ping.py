ced7b0623b19d37b9be896d4bdb6b96163732302
Alex de Wekker <alexdewekker6@gmail.com>
2019-03-30 15:38:37 +0100

    working ping script, still need to add sqlite

--- ping.py
+++ ping.py
@@ -2,15 +2,14 @@
 
 import requests
 import os
+import subprocess
 
-hostname = "192.168.1.77"
+hostname = "192.168.1.99"
 response = os.system("ping -c 1 " + hostname)
 
 if response == 0:
-    pingstatus = "Pcaats is on"
-    # doe iets
-    print("ja")
+    print("Connection found.")
+    subprocess.call(["ledstrip", "on"])
 else:
-    pingstatus = "Pcaats is off"
-    # doe nog iets
-    print("nee")
+    print("Connection lost")
+    subprocess.call(["ledstrip", "off"])
