7589b5c69740273790cd924ce976032eb8a2f7d9
Alex de Wekker <alexdewekker6@gmail.com>
2019-06-01 16:39:39 +0200

    added js models

---
+++ models/weight.js
@@ -0,0 +1,20 @@
+const { Client } = require("pg")
+require("dotenv").config({ path: "/var/www/html/backup/projects/weight/1-public/.env" })
+const query = `
+create table if not exists weight (
+	id serial primary key not null,
+	weight int not null,
+	date date unique not null,
+	notes varchar(255))
+`
+
+const connStr = "postgres://" + process.env.PSQLU + ":" + process.env.PSQLW + "@192.168.1.90:5432/dekelder"
+const client = new Client({ connectionString: connStr })
+client.connect()
+client.query(query).then(data => {
+	console.log(data)
+	client.end()
+}).catch( e=> {
+	console.error(e.stack)
+	client.end()
+})
